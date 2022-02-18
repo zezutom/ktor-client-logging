18990a5692da264655272dbbaa0d92cb000ac597
Tomáš Zezula <tomas.zezula@avast.com>
2022-02-18 23:14:57 +0100

    chore: Unit tests refactoring.

--- src/test/kotlin/com/tomaszezula/ktor/client/logging/ClientLoggingBasicTest.kt
+++ src/test/kotlin/com/tomaszezula/ktor/client/logging/ClientLoggingBasicTest.kt
@@ -1,9 +1,7 @@
 package com.tomaszezula.ktor.client.logging
 
 import ch.qos.logback.classic.Level
-import com.tomaszezula.ktor.client.tracing.TraceId
 import junit.framework.TestCase.assertFalse
-import junit.framework.TestCase.assertTrue
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
@@ -21,19 +19,6 @@ class ClientLoggingBasicTest : TestBase() {
         verifyResponse(Level.DEBUG)
     }
     
-    @Test
-    fun logsTraceId() = runBlocking {
-        val traceId = TraceId.generate()
-        withRequest(traceId = traceId) {
-            memoryAppender.getAllEvents().forEach { event ->
-                assertTrue(
-                    "Trace ID should be part of MDC!",
-                    event.mdcPropertyMap["traceId"] == traceId.value
-                )
-            }
-        }
-    }
-
     @Test
     fun disablesRequestLogging() = runBlocking {
         withRequest(httpClient().config {
