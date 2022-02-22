4383ad570be0264cd7a9b50ae9ef15b085848f5b
Tomas Zezula <zezulatomas@gmail.com>
2022-02-23 00:43:58 +0100

    Feat/codecov (#3)
    feat: Code coverage plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,10 +6,22 @@ val logback_version: String by project
 plugins {
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.serialization") version "1.6.10"
+    jacoco
 }
 
 group = "com.example"
 version = "0.0.1"
+
+// https://gist.github.com/huuphuoc1396/497c27c1fac205f5d9c6696016227e1c
+configurations.all {
+    resolutionStrategy {
+        eachDependency {
+            if ("org.jacoco" == requested.group) {
+                useVersion("0.8.7")
+            }
+        }
+    }
+}
 
 repositories {
     mavenCentral()
@@ -24,4 +36,14 @@ dependencies {
     testImplementation("io.ktor:ktor-client-serialization:$ktor_version")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+    reports {
+        xml.required.set(true)
+    }
 }
